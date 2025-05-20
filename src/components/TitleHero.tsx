cc440367012b77926f6618ea91449654ae2ea58c
s4nj1th <sanjith@tutamail.com>
2025-05-20 14:12:03 +0530

    fixed title hero

--- src/components/TitleHero.tsx
+++ src/components/TitleHero.tsx
@@ -5,7 +5,7 @@ import SocialLinks from "./SocialLinks";
 export default function TitleHero() {
   return (
     <div className="relative w-full mt-[5vh] rounded-[12px] shadow-4xl border border-[#222] bg-[#1118] p-10">
-      <div className="w-[80%] min-w-[300px] flex flex-col md:flex-row items-center justify-center gap-6 md:gap-x-16 mx-auto align-center">
+      <div className="flex flex-col md:flex-row items-center justify-center gap-6 md:gap-x-16 mr-10 align-center">
         <div className="text-white text-center md:text-right md:flex-grow max-w-[900px]">
           <TitleText />
         </div>
