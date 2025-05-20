6bfcbc92c5d059019d4c9ff5c4a84072e0f51858
s4nj1th <sanjith@tutamail.com>
2025-05-20 15:42:05 +0530

    fixed ratio for the text and image

--- src/components/TitleHero.tsx
+++ src/components/TitleHero.tsx
@@ -5,19 +5,21 @@ import SocialLinks from "./SocialLinks";
 export default function TitleHero() {
   return (
     <div className="relative w-full mt-[5vh] rounded-[12px] shadow-4xl border border-[#222] bg-[#1118] p-10">
-      <div className="flex flex-col md:flex-row items-center justify-center gap-6 md:gap-x-16 md:mr-10 align-center">
-        <div className="text-white text-center md:text-right md:flex-grow max-w-[900px]">
+      <div className="flex flex-col md:flex-row items-center justify-between gap-6 mx-auto w-full max-w-[1200px]">
+        <div className="w-full md:w-[60%] text-white text-center md:text-right">
           <TitleText />
         </div>
 
-        <div className="flex flex-col md:flex-row items-center gap-4 shadow-2xl">
-          <Image
-            src="/assets/profile.webp"
-            alt="Profile"
-            width={250}
-            height={250}
-            className="rounded-xl object-contain"
-          />
+        <div className="w-full md:w-[40%] flex flex-col md:flex-row items-center gap-4">
+          <div className="relative w-[250px] h-[250px] rounded-xl overflow-hidden shadow-2xl">
+            <Image
+              src="/assets/profile.webp"
+              alt="Profile"
+              fill
+              className="object-contain"
+              sizes="(min-width: 768px) 250px"
+            />
+          </div>
           <SocialLinks />
         </div>
       </div>
